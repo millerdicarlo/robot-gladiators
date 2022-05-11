bb7df3004cd646dfcf921a9e8ec2082508fd3119
millerd <millerdicarlo@gmail.com>
2022-05-11 10:45:26 -0400

    player name and fight alert

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+
+var playerName = window.prompt("What is your robot's name?");
+
+// Fight Function
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+
+
+  
+  fight();
