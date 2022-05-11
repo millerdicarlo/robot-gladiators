28dbc0e55dd71d46ba36953ef35a0924abb825ba
millerd <millerdicarlo@gmail.com>
2022-05-11 13:01:05 -0400

    console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,12 @@
-
+// playerName input
 var playerName = window.prompt("What is your robot's name?");
+
+// alert popup with newly entered playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this does math and log 20
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
 
 // Fight Function
 function fight() {
@@ -9,4 +16,4 @@ function fight() {
 
 
   
-  fight();
+  // fight();
