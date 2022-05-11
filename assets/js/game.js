741b3a7d2447508aca188887013073359081b3e7
millerd <millerdicarlo@gmail.com>
2022-05-11 16:19:40 -0400

    player health status

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,12 +20,28 @@ var fight = function() {
     console.log(
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
+
+  // check enemy's health
+      if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+      }
+      else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+      }
+
   // subtract enemyAttack from playerHealth and use to update playerHealth variable
       playerHealth = playerHealth - enemyAttack;
   // log resulting message to know it worked
       console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-      )
+      );
+  // check player's health
+      if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+      }
+      else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+      }
 };
 
 
