fbe035f10c3690f5ce1131e089045cb289759146
millerd <millerdicarlo@gmail.com>
2022-05-17 15:20:55 -0400

    rounds added

--- assets/js/game.js
+++ assets/js/game.js
@@ -77,8 +77,16 @@ var fight = function(enemyName) {
 
 // execute fight function
 for (var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
+  //player enters game
+  if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+        var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
   //call fight function with enemy bots
   fight(pickedEnemyName);
+  }
+  else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+  }
 };
